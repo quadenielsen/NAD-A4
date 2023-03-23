df3035a1d85343bc72e5fc81122f51d011c329ff
quadenielsen <quade.nielsen@gmail.com>
2023-03-22 21:59:31 -0400

    Added like counter

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -30,11 +30,11 @@ const getData = () => {
                             </div>
                             <div class="card-footer">
                                 <div class="row">
-                                    <div class="col-1">
+                                    <div class="col-2">
                                         <a href="#" class="btn btn-primary">Details</a>
                                     </div>
                                     <div class="col-2">
-                                        <a href="#" class="btn btn-primary">Like</a>
+                                        <a href="#" class="btn btn-primary">${el.liked ? `Unlike (${el.count})`: `Like (${el.count})`}</a>
                                     </div>
                                 </div>
                             </div>
